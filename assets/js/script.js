6088b6e4f9d10c87b51fa5dcd36f0658d277bcf1
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-02 16:48:47 -0700

    Give variables in js file to refer to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,39 @@
 //WHEN the game is over
 //THEN I can save my initials and score
 
+var questions = [
+    {
+        question: "placeholder question 1",
+        choices: ["a","b","c","d"],
+        answer: "a" //have to be the correct answer
+    },
+    {
+        question: "placeholder question 2",
+        choices: ["1","2","3","4"],
+        answer: "1"//have to be the correct answer
+    }, 
+]
+
+var currentQuestion = 0
+
+var startdiv = document.querySelector("#begin");
+var startbtn = document.querySelector("#submit");
+var questiondiv = document.querySelector("#question");
+var questiontext = document.querySelector("#questiontext");
+var choice1 = document.querySelector("#choice-1");
+var choice2 = document.querySelector("#choice-2");
+var choice3 = document.querySelector("#choice-3");
+var choice4 = document.querySelector("#choice-4");
+var scorecard = document.querySelector("#scorecard");
+var score = document.querySelector("#scoretext");
+var initials = document.querySelector("#initials");
+var save = document.querySelector("#endsave");
+
+
+startbtn.addEventListener("click", nextquestion)
+choice1.addEventListener("click", nextquestion)
+choice2.addEventListener("click", nextquestion)
+choice3.addEventListener("click", nextquestion)
+choice4.addEventListener("click", nextquestion)
+
+
