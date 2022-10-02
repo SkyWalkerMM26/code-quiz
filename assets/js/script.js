4d509dd92841e0ca93565e4f3c1e64ee946780c3
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-02 16:18:39 -0700

    enter the content and structure for index.html

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// peusdocode
+// First, let's set our variables to refer to the index.html by document.querySelector().
+// When the User clicks the Start button, the function setInterval starts.
+// Simultaneously there should be a question that appears on the page for the user to start the quiz.
+// These questions are set up by funcitons. 
+// There should be functions to generate the questions and store the results.
+// There should be an addEvent Listener("click", showRespone) t
+// 
+
+//REQUIREMENT ACCORDING TO THE MODULE 4
+//GIVEN I am taking a code quiz
+//WHEN I click the start button
+//THEN a timer starts and I am presented with a question
+//WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and score
+
